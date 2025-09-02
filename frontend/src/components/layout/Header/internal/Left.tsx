e5f7a6c0dd8dff94aeb650157fcf4afc60df517a
akko <criminale@qq.com>
2025-09-02 19:38:09 +0800

    ✨ feat(header): 添加左侧导航链接组件
    新增左侧导航链接组件，提供指向首页的链接，增强页面导航功能。

---
+++ frontend/src/components/layout/Header/internal/Left.tsx
@@ -0,0 +1,9 @@
+import { NavLink } from "react-router";
+
+export const Left = () => {
+  return (
+    <NavLink to="/" className="no-underline text-theme-primary-text">
+      Kisesaki Blog
+    </NavLink>
+  );
+};

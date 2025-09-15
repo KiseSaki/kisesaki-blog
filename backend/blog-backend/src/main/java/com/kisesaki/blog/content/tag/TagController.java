0a62f2c9f3a7e0d3b79956dcb2c569cf0420c656
akko <criminale@qq.com>
2025-09-15 10:08:18 +0800

    ✨ feat(tag): 添加标签控制器和服务的基本结构
    新增 TagController 和 TagService 类，定义标签相关接口和服务逻辑。

--- backend/blog-backend/src/main/java/com/kisesaki/blog/content/tag/TagController.java
+++ backend/blog-backend/src/main/java/com/kisesaki/blog/content/tag/TagController.java
@@ -1,10 +1,21 @@
 package com.kisesaki.blog.content.tag;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
 /**
  * 标签控制器
  * 
  * @author KiseSaki
  */
+@RestController
+@RequestMapping("/tags")
+@Tag(name = "Tag", description = "标签相关接口")
+@RequiredArgsConstructor
 public class TagController {
 
+    private final TagService tagService;
 }
--- backend/blog-backend/src/main/java/com/kisesaki/blog/content/tag/TagService.java
+++ backend/blog-backend/src/main/java/com/kisesaki/blog/content/tag/TagService.java
@@ -1,10 +1,21 @@
 package com.kisesaki.blog.content.tag;
 
+import org.springframework.stereotype.Service;
+
+import com.kisesaki.blog.content.tag.mapper.TagsMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 标签服务
  * 
  * @author KiseSaki
  */
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class TagService {
 
+    private final TagsMapper tagsMapper;
 }

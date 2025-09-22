742dd372b20b7416ec5e6786a7c3c5f23b24c293
akko <criminale@qq.com>
2025-09-22 17:38:47 +0800

    ✨ feat(header): 增强右侧组件的用户身份验证显示逻辑
    根据用户的身份验证状态，动态显示用户头像或登录按钮。

--- frontend/src/components/layout/Header/internal/Right.tsx
+++ frontend/src/components/layout/Header/internal/Right.tsx
@@ -1,15 +1,22 @@
 import { ThemeToggle } from "@/components/common";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui";
+import { Avatar, AvatarFallback, AvatarImage, Button } from "@/components/ui";
+import { useAuth } from "@/hooks";
 
 export const Right = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <div className="flex items-center space-x-4">
       <ThemeToggle className="text-theme-primary-text" />
       {/* TODO 完成登陆后需要头像显示、identifier、fallback的首字母 */}
-      <Avatar className="cursor-pointer">
-        <AvatarImage src="/path/to/image.jpg" alt="User Avatar" />
-        <AvatarFallback identifier="username">U</AvatarFallback>
-      </Avatar>
+      {isAuthenticated ? (
+        <Avatar className="cursor-pointer">
+          <AvatarImage src="/path/to/image.jpg" alt="User Avatar" />
+          <AvatarFallback identifier="username">U</AvatarFallback>
+        </Avatar>
+      ) : (
+        <Button size="sm">登录</Button>
+      )}
     </div>
   );
 };

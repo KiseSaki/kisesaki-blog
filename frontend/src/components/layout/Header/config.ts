f949cb2b85f43387182d081c0e5bf8da039aefa9
akko <criminale@qq.com>
2025-09-22 18:49:47 +0800

    ✨ feat(header): 更新 Header 菜单配置和登录逻辑
    优化了 Header 菜单的配置格式，提升可读性；在 Right 组件中添加了登录跳转功能，增强用户体验。

--- frontend/src/components/layout/Header/config.ts
+++ frontend/src/components/layout/Header/config.ts
@@ -1,25 +1,32 @@
 export interface IHeaderMenu {
-    label: string;
-    path: string;
-    icon?: React.ReactNode;
-    submenu?: IHeaderMenu[];
+  label: string;
+  path: string;
+  icon?: React.ReactNode;
+  submenu?: IHeaderMenu[];
 }
 
+// Header 菜单配置
 export const headerMenusConfig: IHeaderMenu[] = [
-    {
-        label: "首页",
-        path: "/",
-    },
-    {
-        label: "归档",
-        path: "/blog/archive",
-    },
-    {
-        label: "标签",
-        path: "/blog/tag/[tag]",
-    },
-    {
-        label: "关于我",
-        path: "/blog/aboutMe"
-    }
+  {
+    label: "首页",
+    path: "/",
+  },
+  {
+    label: "归档",
+    path: "/blog/archive",
+  },
+  {
+    label: "标签",
+    path: "/blog/tag/[tag]",
+  },
+  {
+    label: "关于我",
+    path: "/blog/aboutMe",
+  },
 ];
+
+// Avatar 跳转链接
+export const AVATAR_LINK = "/user/profile";
+
+// Login 跳转链接
+export const LOGIN_LINK = "/auth/login";
--- frontend/src/components/layout/Header/internal/Right.tsx
+++ frontend/src/components/layout/Header/internal/Right.tsx
@@ -1,9 +1,17 @@
 import { ThemeToggle } from "@/components/common";
 import { Avatar, AvatarFallback, AvatarImage, Button } from "@/components/ui";
 import { useAuth } from "@/hooks";
+import { useNavigate } from "react-router";
+import { LOGIN_LINK } from "../config";
 
 export const Right = () => {
   const { isAuthenticated } = useAuth();
+
+  // 登录跳转
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate(LOGIN_LINK);
+  };
 
   return (
     <div className="flex items-center space-x-4">
@@ -15,7 +23,9 @@ export const Right = () => {
           <AvatarFallback identifier="username">U</AvatarFallback>
         </Avatar>
       ) : (
-        <Button size="sm">登录</Button>
+        <Button size="sm" onClick={handleLogin}>
+          登录
+        </Button>
       )}
     </div>
   );

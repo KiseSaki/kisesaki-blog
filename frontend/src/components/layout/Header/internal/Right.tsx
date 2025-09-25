f09f65b84338f5665d5fc2d3b4bbde7696ab5d1e
akko <criminale@qq.com>
2025-09-25 15:49:20 +0800

    ✨ feat(Right): 添加用户头像和下拉菜单功能
    实现了用户头像的显示，支持个人主页跳转和退出登录功能，增强了用户体验。

--- frontend/src/components/layout/Header/internal/Right.tsx
+++ frontend/src/components/layout/Header/internal/Right.tsx
@@ -1,11 +1,19 @@
-import { Avatar, AvatarFallback, AvatarImage, Button } from '@/components';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Button,
+  HoverCard,
+  HoverCardContent,
+} from '@/components';
 import { ThemeToggle } from '@/components/common';
 import { useAuth } from '@/hooks';
+import { HoverCardTrigger } from '@radix-ui/react-hover-card';
 import { useNavigate } from 'react-router';
 import { LOGIN_LINK } from '../config';
 
 export const Right = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, user, logout } = useAuth();
 
   // 登录跳转
   const navigate = useNavigate();
@@ -13,15 +21,39 @@ export const Right = () => {
     navigate(LOGIN_LINK);
   };
 
+  // 个人主页跳转
+  const handleProfile = () => {
+    navigate('/user/profile');
+  };
+
+  // 退出登录
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  };
+
   return (
     <div className='flex items-center space-x-4'>
       <ThemeToggle className='text-theme-primary-text' />
-      {/* TODO 完成登陆后需要头像显示、identifier、fallback的首字母 */}
       {isAuthenticated ? (
-        <Avatar className='cursor-pointer'>
-          <AvatarImage src='/path/to/image.jpg' alt='User Avatar' />
-          <AvatarFallback identifier='username'>U</AvatarFallback>
-        </Avatar>
+        <HoverCard openDelay={200} closeDelay={200}>
+          <HoverCardTrigger asChild>
+            <Avatar className='cursor-pointer' onClick={handleProfile}>
+              <AvatarImage src={user?.avatarUrl} alt='User Avatar' />
+              <AvatarFallback identifier={user?.displayName}>U</AvatarFallback>
+            </Avatar>
+          </HoverCardTrigger>
+          <HoverCardContent className='w-auto'>
+            <div className='flex flex-col space-y-2'>
+              <Button variant='ghost' onClick={handleProfile}>
+                个人主页
+              </Button>
+              <Button variant='ghost' onClick={handleLogout}>
+                退出登录
+              </Button>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
       ) : (
         <Button size='sm' onClick={handleLogin}>
           登录

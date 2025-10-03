6fa6a7c63f90d94b74d5b7759c286019e27216bf
akko <criminale@qq.com>
2025-10-03 15:44:42 +0800

    ✨ feat(HomePage): 完成首页的HeroSection部分

---
+++ frontend/src/pages/Blog/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import { HeroSection } from './HeroSection';
+
+const HomePage = () => {
+  return (
+    <div className='px-64'>
+      <HeroSection />
+    </div>
+  );
+};
+
+export default HomePage;
